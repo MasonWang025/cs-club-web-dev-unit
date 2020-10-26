654374e00406605da862a05b23d1498ada56cdb5
MasonWang025 <mason.wang0025@gmail.com>
2020-10-26 13:56:48 -0700

    meeting 2

--- 2. HTML, CSS, JS basics/index.js
+++ 2. HTML, CSS, JS basics/index.js
@@ -1,1 +1,11 @@
 // createElement(), appendChild()
+let input = document.getElementById("text-input");
+let btn = document.getElementById("add-button");
+let todo = document.getElementById("todo");
+
+btn.onclick = () => {
+  let li = document.createElement("li");
+  li.textContent = input.value;
+  todo.appendChild(li);
+  input.value = "";
+};
